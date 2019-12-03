ffaf8e9e9e221e802108437f016e39bdd0177031
yadhira feliz <yara09feliz@hotmail.com>
2019-12-03 19:50:05 -0400

    Creacion de los modelos y tablas borrados anteriormente

---
+++ FinalProgramacion/FinalProgramacion/Controllers/VacacionesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FinalProgramacion.Models;
+
+namespace FinalProgramacion.Controllers
+{
+    public class VacacionesController : Controller
+    {
+        private FPEntities db = new FPEntities();
+
+        // GET: Vacaciones
+        public ActionResult Index()
+        {
+            return View(db.Vacaciones.ToList());
+        }
+
+        // GET: Vacaciones/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vacaciones vacaciones = db.Vacaciones.Find(id);
+            if (vacaciones == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vacaciones);
+        }
+
+        // GET: Vacaciones/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Vacaciones/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "empleado,desde,hasta,correspondiente_año_,comentario")] Vacaciones vacaciones)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Vacaciones.Add(vacaciones);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(vacaciones);
+        }
+
+        // GET: Vacaciones/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vacaciones vacaciones = db.Vacaciones.Find(id);
+            if (vacaciones == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vacaciones);
+        }
+
+        // POST: Vacaciones/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "empleado,desde,hasta,correspondiente_año_,comentario")] Vacaciones vacaciones)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vacaciones).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(vacaciones);
+        }
+
+        // GET: Vacaciones/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vacaciones vacaciones = db.Vacaciones.Find(id);
+            if (vacaciones == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vacaciones);
+        }
+
+        // POST: Vacaciones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Vacaciones vacaciones = db.Vacaciones.Find(id);
+            db.Vacaciones.Remove(vacaciones);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
